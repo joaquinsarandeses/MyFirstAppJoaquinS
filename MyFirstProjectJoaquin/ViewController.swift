153375ea87613b35b5fdf3a2e4d8fb377f99eaf4
joaquin <joaquin_sarandeses_m-tsapp1ta2122@cev.com>
2022-09-22 20:48:09 +0200

    first tries

--- MyFirstProjectJoaquin/ViewController.swift
+++ MyFirstProjectJoaquin/ViewController.swift
@@ -9,9 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        numberAnalysis()
+        numberAnalysis2()
+        
+        for i in 1...5 {
+            print(i)
+            
+        }
+        
+        var o = 1
+        
+        while o < 6{
+            print(o)
+            o += 1
+        }
+        
+        var u = 1
+        
+        repeat {
+            print (u)
+            u += 1
+            
+        } while u < 6
+        
+        
+        
+        
+        
+        
+        
+            
+        
+    }
+    
+    func numberAnalysis2(){
+        let nota = Int.random(in: 0..<10)
+        
+        switch nota{
+            
+        case 0...4:
+            print("suspenso")
+            break
+        case 5,6:
+            print("bien")
+            break
+        case 7,8:
+            print("notable")
+            break
+        case 9,10:
+            print("Sobresaliente")
+            break
+        
+        default:
+            print("come caca")
+            break
+    }
+    }
+    
+    func numberAnalysis(){
+        
+        let numero = Int.random(in: 1..<3)
+    
+        if numero < 2  {
+            print("soy pequeño")
+        }else if numero >= 2 {
+            print("soy mediano")
+        }else if numero >= 3{
+            print("soy grande")
+        }
     }
 
 
